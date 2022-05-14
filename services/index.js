ce8ba1bab39745d518a04e14ea873d30eb19427d
xly <328704719@qq.com>
2022-05-14 19:40:06 +0800

    feat: 中间件添加

--- services/index.js
+++ services/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 require('dotenv').config({
   path: process.env.NODE_ENV ? `./.env.${process.env.NODE_ENV}` : './.env',
 });
@@ -9,13 +11,18 @@ const initRouter = require('./src/routes');
 
 const app = express();
 
+// 中间件
+app.use(cors()); // 跨域
+app.use(express.json()); // 解析
+app.use(morgan('short')); // 请求日志
+
 const main = async () => {
   // 连接数据库
   await sqlConnect();
-  // 初始化路由
-  initRouter(app);
   // 启动服务
   await initServer(app);
+  // 初始化路由
+  initRouter(app);
 };
 
 main();
