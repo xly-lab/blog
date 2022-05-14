f6726d0b7899679774daeb5b0c2b67f94e5c0f5e
xly <328704719@qq.com>
2022-05-14 18:18:12 +0800

    feat: 优化模块化路径 - 1

--- services/index.js
+++ services/index.js
@@ -2,18 +2,14 @@ require('dotenv').config({
   path: process.env.NODE_ENV ? `./.env.${process.env.NODE_ENV}` : './.env',
 });
 const sqlConnect = require('./init/sql');
-const sequelize = require('./init/sql/sequelize');
 const initServer = require('./init/server');
 const express = require('express');
-const associateThem = require('./init/sql/models/associateThem');
 
 const app = express();
 
 const main = async () => {
   // 连接数据库
   await sqlConnect();
-  associateThem();
-  await sequelize.sync();
   // 启动服务
   await initServer(app);
 };
--- services/init/sql/index.js
+++ services/init/sql/index.js
@@ -1,8 +1,11 @@
 const sequelize = require('./sequelize');
+const associateThem = require('./models/associateThem');
 
 const sqlConnect = async () => {
   try {
     await sequelize.authenticate();
+    associateThem();
+    await sequelize.sync({ alter: true });
     console.log('Connection mysql has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database of mysql:', error);
