e544252793e42ee13853a6f2f8c565bf226834a9
xly <328704719@qq.com>
2022-05-23 20:48:43 +0800

    feat: 添加评论的增加删除查询 接口

--- services/src/controller/comment/index.js
+++ services/src/controller/comment/index.js
@@ -4,7 +4,33 @@ const Article = require('../../init/sql/models/article');
 
 // 创建评论
 const createComment = async (req, res) => {
+  const loggedUser = req.loggedUserInfo || {};
+  const { slug = '', body = '' } = req.body || {};
   try {
+    const article = await Article.findByPk(slug);
+    if (!article) {
+      res.status(400).json({
+        code: 0,
+        message: '当前文章不存在',
+      });
+      return;
+    }
+    const createResult = await Comment.create({
+      body,
+      UserEmail: loggedUser.email,
+      ArticleSlug: slug,
+    });
+    if (!createResult) {
+      res.status(400).json({
+        code: 0,
+        message: '评论失败',
+      });
+      return;
+    }
+    res.status(200).json({
+      code: 0,
+      message: '评论成功',
+    });
   } catch (error) {
     res.status(500).json({
       code: 0,
@@ -16,6 +42,29 @@ const createComment = async (req, res) => {
 // 获取文章所有评论
 const getCommentList = async (req, res) => {
   try {
+    const { slug = '', limit = 10, offset = 0 } = req.body || {};
+    const article = await Article.findByPk(slug);
+    if (!article) {
+      res.status(400).json({
+        code: 0,
+        message: '当前文章不存在',
+      });
+      return;
+    }
+    const comments = await Comment.findAndCountAll({
+      where: { ArticleSlug: slug },
+      limit: Number(limit),
+      offset: Number(offset),
+    });
+    const total = comments?.count ?? 0;
+    res.status(200).json({
+      code: 1,
+      message: '查询成功',
+      data: {
+        total,
+        comments: comments?.rows || [],
+      },
+    });
   } catch (error) {
     res.status(500).json({
       code: 0,
@@ -26,7 +75,28 @@ const getCommentList = async (req, res) => {
 
 // 删除评论
 const deleteComment = async (req, res) => {
+  const { id = '' } = req.body;
   try {
+    const comment = await Comment.findByPk(id);
+    if (!comment) {
+      res.status(400).json({
+        code: 0,
+        message: '评论不存在',
+      });
+      return;
+    }
+    const deleteResult = await comment.destroy();
+    if (!deleteResult) {
+      res.status(400).json({
+        code: 0,
+        message: '删除失败',
+      });
+      return;
+    }
+    res.status(200).json({
+      code: 1,
+      message: '删除成功',
+    });
   } catch (error) {
     res.status(500).json({
       code: 0,
--- services/src/init/sql/models/comment.js
+++ services/src/init/sql/models/comment.js
@@ -2,6 +2,11 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../sequelize');
 
 const Comment = sequelize.define('Comment', {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    defaultValue: DataTypes.UUIDV1,
+  },
   // 在这里定义模型属性
   body: {
     // 内容
