df834fd23415cb5197490229938dc7305e1a74cb
xly <328704719@qq.com>
2022-05-29 16:38:02 +0800

    feat: 优化

--- client/src/layout/components/Footer/index.tsx
+++ client/src/layout/components/Footer/index.tsx
@@ -3,8 +3,8 @@ import style from './index.module.scss';
 
 export default function Footer() {
   return (
-    <div className="w-full bg-blue-300 flex flex-col justify-around items-center h-16">
-      <div className={`${style.fontStyle} w-max`}>只需要改一行代码，不会影响其它程序的。</div>
+    <div className="w-full bg-blue-300 flex flex-col justify-around items-center h-16 overflow-hidden ">
+      <div className={`${style.fontStyle} w-96 truncate text-center`}>只需要改一行代码，不会影响其它程序的。</div>
       <div>Powered react ;no dream</div>
     </div>
   );
